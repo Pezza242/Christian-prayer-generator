f0a963960f0b5205e218db30603cdf1df03ddc08
Pezza242 <periyearwood@gmail.com>
2025-06-05 17:06:55 +0100

    Started API Integration

--- index.js
+++ index.js
@@ -11,9 +11,18 @@ function generatePrayer(event) {
 let prayerForm = document.querySelector("#prayer-form");
 prayerForm.addEventListener("submit", generatePrayer);
 
+function shecodesAI(){
+let prompt = "";
+let context = "";
+let apiKey = "eb79bof31898546ffea432d4bb90t390";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`:
+}
+
+
 new Typewriter("#prayer-answer", {
   strings:
     " Your prayer will appear here. <br/> God bless you on your prayer journey to become closer to God. x",
   autoStart: true,
   delay: 60,
 });
+

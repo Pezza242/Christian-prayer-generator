13e0d1b7e056214f0187fa27dc95b1e5e33af5b3
Pezza242 <periyearwood@gmail.com>
2025-06-05 18:17:47 +0100

    Intregated AI API into JS
    Made sure the context and prompt specifically to do with Christian scriptures and prayers

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-function generatePrayer(event) {
-  event.preventDefault();
+function generatePrayer(response) {
+  let answer = response.data.answer;
+
   new Typewriter("#prayer-answer", {
-    strings:
-      "God, thank you for everything. Help me to be a better person today.",
+    strings: answer,
     autoStart: true,
     delay: 60,
   });
 }
+function getPrayer(event) {
+  event.preventDefault();
+  let searchKeyword = document.querySelector("#search-bar");
 
-let prayerForm = document.querySelector("#prayer-form");
-prayerForm.addEventListener("submit", generatePrayer);
+  let context =
+    "You are learning about Christianity so you can help other christians with their religious journey. Your task is to generate a short prayer based the topic they have picked. Your can add scriptures about the topic too if you want. Make sure to follow user's instructions.";
+  let prompt = `User instructions: Generate a prayer about ${searchKeyword.value}`;
+  let apiKey = "eb79bof31898546ffea432d4bb90t390";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-function shecodesAI(){
-let prompt = "";
-let context = "";
-let apiKey = "eb79bof31898546ffea432d4bb90t390";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`:
+  axios.get(apiUrl).then(generatePrayer);
 }
 
+let prayerForm = document.querySelector("#prayer-form");
+prayerForm.addEventListener("submit", getPrayer);
 
 new Typewriter("#prayer-answer", {
   strings:
@@ -25,4 +29,3 @@ new Typewriter("#prayer-answer", {
   autoStart: true,
   delay: 60,
 });
-
